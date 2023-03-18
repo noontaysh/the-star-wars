fb106aadb43d105c6df92454609ca63f8ca5d42f
Johnny <n00ntaysh@gmail.com>
2023-03-18 10:02:02 +0200

    style: add styling for planet profile/excerpt component
    Everything is the same as with the characters, now the profile of the
    planet is more pleasant. In addition to this, two style classes have
    been added as variables to avoid copying the same code when styling the
    component profile.

--- src/features/planets/planetProfile/PlanetExcerpt.jsx
+++ src/features/planets/planetProfile/PlanetExcerpt.jsx
@@ -1,20 +1,21 @@
 import React from 'react';
 import {numberWithCommas} from "../../../utilities/numbersWithComas";
+import './styles/PlanetExcerpt.scss'
 
 const PlanetExcerpt = (props) => {
     return (
-        <div className={''}>
+        <div className={'planetProfile'}>
             <img src={`https://starwars-visualguide.com./assets/img/planets/${props.planetId}.jpg`} alt=""
                  onError={(e) => {
                      e.target.onError = null;
                      e.target.src = `https://starwars-visualguide.com/./assets/img/placeholder.jpg`
                  }}/>
-            <div className={''}>
+            <div className={'planetProfile__content'}>
                 <h1>{props.name}</h1>
                 <p>Population: {numberWithCommas(props.population)}</p>
-                <p>Diameter: {props.diameter}{props.diameter !== 'unknown' ? 'km' : ''}</p>
-                <p>Rotation Period: {props.rotation_period} {props.rotation_period !== 'unknown' ? 'days' : ''}</p>
-                <p>Orbital Period: {props.orbital_period} {props.orbital_period !== 'unknown' ? 'days' : ''}</p>
+                <p>Diameter: {numberWithCommas(props.diameter)}{props.diameter !== 'unknown' ? 'km' : ''}</p>
+                <p>Rotation Period: {numberWithCommas(props.rotation_period)} {props.rotation_period !== 'unknown' ? 'days' : ''}</p>
+                <p>Orbital Period: {numberWithCommas(props.orbital_period)} {props.orbital_period !== 'unknown' ? 'days' : ''}</p>
                 <p>Gravity: {props.gravity}</p>
                 <p>Surface Water: {props.surface_water}{props.surface_water !== 'unknown' ? '%' : ''}</p>
                 <p>Terrain: {props.terrain}</p>
